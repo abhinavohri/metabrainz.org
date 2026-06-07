cfc3455f5efae3ea9d52d48191d8bfd267ce0879
Abhinav Ohri <abhinavohri13@gmail.com>
2026-06-07 23:53:35 +0530

    claenup comments

--- metabrainz/babel.py
+++ metabrainz/babel.py
@@ -4,6 +4,4 @@ from metabrainz.i18n import get_locale
 
 
 def init_app(app):
-    # Flask-Babel calls this selector per request, so both apps can share the
-    # same cookie/header locale behavior.
     Babel(app, locale_selector=get_locale)
